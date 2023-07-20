f23467f72e1fda9ce000dc208b4b0cc26fe59857
[Araind Kumar] <[20r15a0510@gcet.edu.in]>
2023-07-20 19:08:41 +0530

    mq6

--- src/components/Cart/Cartpage.js
+++ src/components/Cart/Cartpage.js
@@ -1,75 +1,96 @@
-import React from "react";
+import React, { useState } from "react";
 import "./cartpage.css";
 import cartimg from "../assests/men formal 2.jpg";
-const cartpage = () => {
+const Cartpage = () => {
+  const [count, setcount] = useState(1);
+  const handleIncrement = () => {
+    setcount((prevCount) => prevCount + 1);
+  };
+
+  const handleDecrement = () => {
+    setcount((prevCount) => Math.max(prevCount - 1, 1));
+  };
   return (
     <div className="cart-container">
-      <div className="cart-cardbox">
+      <div className="cart-box">
         <div className="cart-productbox">
           <div className="productimg">
-            {" "}
-            <img src={cartimg} alt="cmg" />
+            <img src={cartimg} alt="cartimg" />
           </div>
-          <div className="product-detailsbox">
-            <h3
-              style={{
-                fontWeight: 500,
-                letterSpacing: "0em",
-                textAlign: "left",
-              }}
-            >
-              Men Formal Shirt
-            </h3>
-            <h5
-              style={{
-                fontWeight: 700,
-                fontSize: "25px",
-              }}
-            >
-              $200
-            </h5>
-            <div className="pd2">
-              <h3>50% off</h3>
-              <h4 style={{ fontWeight: 400, fontSize: "13px" }}>Quantity:1</h4>
-              <button style={{ fontWeight: 600 }} className="rbtn">
-                Remove from cart
-              </button>
-              <button style={{ fontWeight: 600 }} className="mbtn">
-                Move to Wishlist
-              </button>
+          <div className="product-content">
+            <div className="productinfo">
+              <h3
+                style={{
+                  fontWeight: 600,
+                  lineHeight: "36px",
+                  letterSpacing: "0em",
+                  textAlign: "left",
+                }}
+              >
+                Men Formal Shirt
+              </h3>
+              <h5
+                style={{
+                  fontWeight: 1000,
+                  fontSize: "20px",
+                }}
+              >
+                $200
+              </h5>
+              <h3
+                style={{
+                  fontWeight: 400,
+                  fontSize: "15px",
+                  marginTop: "-8rem",
+                }}
+              >
+                50% off
+              </h3>
+              <div>
+                Quantity:
+                <button className="qi" onClick={handleIncrement}>
+                  +
+                </button>
+                {count}
+                <button className="qd" onClick={handleDecrement}>
+                  -
+                </button>
+              </div>
+            </div>
+            <div className="product-buttons">
+              <button className="rbtn">Remove from cart</button>
+              <button className="mbtn">Move to Wishlist</button>
             </div>
           </div>
         </div>
         <div className="cart-pricebox">
-          <h1 style={{ fontSize: "24px", fontWeight: 700, lineHeight: 3 }}>
-            PRICE DETAILS
-          </h1>
-          <div className="line"></div>
-          <h2>
-            Price(1 items) <span className="price">$200</span>
-          </h2>
-          <h2>
-            Discount <span className="discount">$100</span>
-          </h2>
-          <h2>
-            Delivery charges <span className="delcharge">free</span>
-          </h2>
-          <div className="line"></div>
-          <h1>
-            Total Amount <span className="total">$100</span>
-          </h1>
-          <div className="line"></div>
-          <h2 style={{ fontSize: "20px" }}>You will save $100 on this order</h2>
-          <br />
-          <button className="plo">
-            <h2 style={{ fontSize: "20px", textAlign: "center" }}>
-              Place Order{" "}
+          <div className="pricebox-content">
+            <h1>PRICE DETAILS</h1>
+            <div className="line"></div>
+            <h2>
+              Price(1 items) <span className="price">$200</span>
             </h2>
-          </button>
+            <h2>
+              Discount <span className="price1">$100</span>
+            </h2>
+            <h2>
+              Delivery charges <span className="dcharge">free</span>
+            </h2>
+            <div className="line"></div>
+            <h1>
+              Total Amount <span className="total">$100</span>
+            </h1>
+            <div className="line"></div>
+            <h2>You will save $100 on this order</h2>
+            <br />
+            <button>
+              <h2>Place Order</h2>
+            </button>
+          </div>
         </div>
       </div>
     </div>
   );
 };
 
-export default cartpage;
+export default Cartpage;
--- src/components/Productpage/productpage.js
+++ src/components/Productpage/productpage.js
@@ -6,6 +6,7 @@ import p3image from "../assests/menformal.jpg";
 import p4image from "../assests/men formal 2.jpg";
 import p5image from "../assests/menredchecks.jpg";
 import p6image from "../assests/download.jpg";
+import { Link } from "react-router-dom";
 const productpage = () => {
   const products = [
     {
@@ -144,7 +145,9 @@ const productpage = () => {
                 <div className="productcontent">
                   <p>{product.name}</p>
                   <h1>${product.price}</h1>
-                  <button className="addcartbtn">Add to Cart</button>
+                  <Link to="/cart">
+                    <button className="addcartbtn">Add to Cart</button>
+                  </Link>
                 </div>
               </div>
             ))}
@@ -156,89 +159,3 @@ const productpage = () => {
 };
 
 export default productpage;
-
-/*import React from "react";
-import "./productpage.css";
-import pimage from "../assests/pimages.jpg";
-import p2image from "../assests/2download.jpg";
-import p3image from "../assests/menformal.jpg";
-import p4image from "../assests/men formal 2.jpg";
-import p5image from "../assests/menredchecks.jpg";
-import p6image from "../assests/download.jpg";
-import prange from "../assests/pricerange.jpg";
-import { Link } from "react-router-dom";
-
-const ProductPage = () => {
-  
-  return (
-    <div>
-      <div className="product-container">
-        <div className="sidebar">
-          <div className="header">
-            <h5>Filter</h5>
-            <h5>Clear</h5>
-          </div>
-
-          <div className="sidebar-content">
-            <div className="pricerange">
-              {" "}
-              <h5>Price</h5>
-              <img src={prange} alt="primg" />
-            </div>
-            <div className="category">
-              <h5>Category</h5>
-              <div className="inp1">
-                {" "}
-                Men
-                <input type="checkbox" />
-                <br />
-                kids
-                <input type="checkbox" />
-              </div>
-            </div>
-            <div className="rating">
-              <h5>Rating</h5>
-              <div className="ritems">
-                <i class="bi bi-record-circle-fill"> 4stars</i>
-                <i class="bi bi-record-circle-fill"> 3stars</i>
-                <i class="bi bi-record-circle-fill"> 2stars</i>
-                <i class="bi bi-record-circle-fill"> 1stars</i>
-              </div>
-            </div>
-            <div className="sorting">
-              <h5>Sorting</h5>
-              <div className="sitems">
-                <i class="bi bi-record-circle-fill"> Price-low to high</i>
-                <i class="bi bi-record-circle-fill"> Price-high to low</i>
-              </div>
-            </div>
-          </div>
-        </div>
-        
-
-        <div className="products">
-          <div className="phead">
-            <h3>Showing All Products</h3>
-          </div>
-
-          {products.map((product) => (
-            <div className="pcard1" key={product.id}>
-              <div className="productimgbox">
-                {" "}
-                <img src={product.image} alt="pimg" />
-              </div>
-              <p>{product.name}</p>
-              <h1>${product.price}</h1>
-
-              <Link to="/cart">
-                <button className="addcartbtn">Add to Cart</button>
-              </Link>
-            </div>
-          ))}
-        </div>
-      </div>
-    </div>
-  );
-};
-
-export default ProductPage; */
